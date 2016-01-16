df0e21840e7913e452aa364b9e1fe114adba8254
Noe Sanchez <mothership@Noes-iMac.local>
2016-01-16 13:59:14 -0600

    color palette generated

---
+++ Gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp'),
+	$ = require('gulp-load-plugins')();
+
+var path = {
+	styles : {
+		src : './source/sass/**/*.scss',
+		dest : './stylesheet/'
+	}
+}
+
+gulp.task('styles', function () {
+	return gulp.src(path.styles.src)
+		.pipe($.sass().on('error', $.sass.logError))
+		.pipe(gulp.dest(path.styles.dest));
+});
+
+gulp.task('watch', function () {
+	gulp.watch(path.styles.src , ['styles']);
+});
+
+
+gulp.task('default', ['styles','watch']);
